53221cd74276e0c53363d77731e278ee53c0d989
gmulz <themulitzer@gmail.com>
2017-08-20 21:59:58 -0400

    Updated UI and added csv parsing

--- budget-tracker/src/App.js
+++ budget-tracker/src/App.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FileInput from 'react-file-input';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.data = [];
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>I am an immature weenie</h2>
+          <h1> Budget Tracker </h1>
+          <form>
+            <input id="upload" type="file" accept=".csv" onChange={this.parseCSV.bind(this)} />
+            <label htmlFor="upload"> Choose Budget</label>
+
+          </form>
         </div>
-        <p className="App-intro">
-          Sorry about the boobs comment in the earlier commit
-        </p>
       </div>
     );
   }
+
+  refreshData(data){
+    this.data = data;
+    console.log(this.data);
+  }
+
+  parseCSV(event){
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    var self = this;
+    reader.onload = function(progressEvent){
+      var text = this.result;
+      var lines = text.split('\n');
+      //Parse first line
+      var header = lines[0];
+      var headings = header.split(',');
+      var data = [];
+      for (var headingNum = 0; headingNum < headings.length; headingNum += 2){
+        var heading = headings[headingNum];
+        var categoryObj = {
+          name: heading,
+          transactions: []
+        };
+        data.push(categoryObj);
+      }
+      //Parse rest
+      for (var lineNum = 1; lineNum < lines.length; lineNum++){
+        var line = lines[lineNum];
+        var elements = line.split(',');
+        for(var elNum = 0; elNum < elements.length; elNum += 2){
+          var d = elements[elNum];
+          if(!d){
+            continue;
+          }
+          var c = elements[elNum + 1];
+          var transaction = {
+            title: d,
+            cost: c
+          }
+          data[elNum/2].transactions.push(transaction);
+        }
+      }
+      //call reload page data
+      self.refreshData(data);
+    }
+    reader.readAsText(file);
+  }
+
 }
 
 export default App;
